fcc292fb3846f3ea5fb64a7e1db21b86cec216f8
wasiflamer <wasiflamer@gmail.com>
2024-02-17 04:07:57 +0500

    tailwind added

--- DesignOne/src/App.jsx
+++ DesignOne/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold ml-5 mt-5">Hello world!</h1>
+      <button className="btn btn-outline btn-success ml-5 mt-2">Success</button>
+    </>
+  );
 }
 
 export default App;
