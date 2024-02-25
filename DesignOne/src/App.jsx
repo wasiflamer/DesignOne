a5c8a390bcc47b8567c463970f2de8df0313b3f0
wasiflamer <wasiflamer@gmail.com>
2024-02-25 18:20:53 +0500

    major improvement

--- DesignOne/src/App.jsx
+++ DesignOne/src/App.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
 import "./App.css";
 
 import contrast_svg1 from "./assets/Dark _analytics-bro.svg";
 import contrast_svg2 from "./assets/Visual data-bro.svg";
 import contrast_svg3 from "./assets/Analysis.svg";
+import contrast_svg4 from "./assets/Software_integration-bro.svg";
 
 function App() {
   return (
@@ -25,20 +27,29 @@ export default App;
 function HeroSection() {
   return (
     <>
-      <div className="hero min-h-screen bg-base-200">
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://pattern8.com/images/pattern-thumbs/pattern8-pattern-44a.png)",
+          backgroundSize: "12vh",
+        }}
+      >
         <div className="hero-content text-center">
           <div className="max-w-lg">
-            <h1 className="text-5xl font-bold text-green-500">
+            <h1 className="text-5xl font-bold text-primary">
               Win your dream job
             </h1>
-            <h1 className="text-5xl font-bold">with JobSnap</h1>
-            <p className="py-6 font-medium">
-              Navigate Your Career Journey with JobSnapAI – Where Opportunities
+            <h1 className="text-5xl font-bold text-neutral ">with JobSnap</h1>
+            <p className="py-6 font-medium text-neutral">
+              Navigate Your Career Journey with JobSnap – Where Opportunities
               Meet Intelligence!
             </p>
             <label className="input input-bordered flex items-center gap-2 py-8">
               <input type="text" className="grow" placeholder="Your Email" />
-              <span className=" btn btn-success">Start For Free</span>
+              <span className=" btn btn-accent rounded-s-badge ">
+                Start For Free
+              </span>
             </label>
             <CardSections />
           </div>
@@ -74,7 +85,7 @@ function CardSections() {
 
 function Card({ icon, title, content }) {
   return (
-    <div className="card w-96 bg-neutral text-neutral-content ">
+    <div className="card w-96 bg-white border-primary border-l-accent border-l-8 border-2 text-neutral  rounded-2xl ">
       <div className="card-body text-left">
         <h2 className="card-title">{icon}</h2>
         <p className="font-bold">{title}</p>
@@ -88,19 +99,19 @@ function Card({ icon, title, content }) {
 
 function SecondaryHero() {
   return (
-    <div className="m-10 ">
-      <div className="hero min-h-screen bg-green-500 rounded-lg">
+    <div className="m-20 ">
+      <div className="hero min-h-screen bg-primary text-secondary rounded-lg p-20">
         <div className="hero-content text-center">
           <div className="max-w-3xl">
-            <h1 className="text-5xl font-bold text-black">
+            <h1 className="text-5xl font-bold">
               Submiting Better Job Applications
             </h1>
-            <h1 className="text-9xl font-bold text-black">10X Faster</h1>
-            <p className="py-6 text-black">
+            <h1 className="text-9xl font-bold">10X Faster</h1>
+            <p className="py-6 font-sans text-2xl text-secondary">
               AI Cover letter Generator, Resume keyword checker, outreach
               Message writer and more powered by GPT.
             </p>
-            <button className="btn ">Image Here</button>
+            <img src={contrast_svg4} alt="" />
           </div>
         </div>
       </div>
@@ -110,7 +121,7 @@ function SecondaryHero() {
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-secondary">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -166,8 +177,8 @@ function Navbar() {
         </ul>
       </div>
       <div className="navbar-end gap-2">
-        <a className="btn btn-neutral">Sign in</a>
-        <a className="btn btn-success">Get Started</a>
+        <a className="btn btn-white rounded-s-badge">Sign in</a>
+        <a className="btn btn-accent rounded-e-badge">Get Started</a>
       </div>
     </div>
   );
@@ -295,7 +306,7 @@ function ContrastHero() {
           </div>
         </div>
         <div className="hero max-w-full h-4/6  bg-grey-500">
-          <div className="hero-content flex-col lg:flex-row m-32">
+          <div className="hero-content flex-col lg:flex-row-reverse m-32">
             <img src={contrast_svg3} alt="Analytics" className="w-3/4" />
             <div>
               <h1 className="text-5xl font-bold text-right text-white ">
@@ -325,23 +336,30 @@ function ContrastHero() {
 function Footer() {
   return (
     <>
-      <footer className="footer p-10 bg-green-950 text-base-content">
+      <footer className="footer p-10 bg-black text-base-content">
         <nav>
-          <h6 className="footer-title">Services</h6>
+          <h6 className="footer-title">Platform</h6>
           <a className="link link-hover">Branding</a>
           <a className="link link-hover">Design</a>
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Company</h6>
+          <h6 className="footer-title">Features</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Resources</h6>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Social</h6>
+          <h6 className="footer-title">Support</h6>
           <div className="grid grid-flow-col gap-4">
             <a>
               <svg
@@ -379,6 +397,11 @@ function Footer() {
           </div>
         </nav>
       </footer>
+      <div className="divider divider-info"></div>
+      <aside className="text-center m-4">
+        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        <a href="https://storyset.com/computer">illustrations by Storyset</a>
+      </aside>
     </>
   );
 }
@@ -386,25 +409,16 @@ function Footer() {
 function ThinLinefooter() {
   return (
     <>
-      <footer className="footer footer-center p-10 bg-green-400 text-primary-content">
+      <footer className="footer footer-center p-10 bg-black text-primary-content">
         <aside>
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            className="inline-block fill-current"
-          >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p className="font-bold">
-            ACME Industries Ltd. <br />
-            Providing reliable tech since 1992
+          <h1 className="font-bold text-7xl">What's Next</h1>
+          <p className="font-semibold">
+            Navigate Your Career Journey with JobSnapAI – Where Opportunities
+            Meet Intelligence!
           </p>
-          <p>Copyright © 2024 - All right reserved</p>
-          <button className="btn btn-wide btn-info">Info</button>
+          <button className="btn btn-wide btn-outline btn-info m-3">
+            Try For free !{" "}
+          </button>
         </aside>
       </footer>
     </>
--- DesignOne/tailwind.config.js
+++ DesignOne/tailwind.config.js
@@ -5,4 +5,26 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          // purple
+          primary: "#9333ea",
+          // white
+          secondary: "#FFFFFF",
+          //grey
+          neutral: "#3d4451",
+
+          // accent
+          // ..cyan one
+
+          // little dim for buttons
+
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+    ],
+  },
 };
